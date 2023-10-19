b3acaab6e82f763b67d5024321bfed837098565c
Meow <tabbathapischke@gmail.com>
2023-10-19 09:55:27 -1000

    format docs

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
-const {Triangle, Square, Circle} = require("./lib/shapes");
+const { Triangle, Square, Circle } = require("./lib/shapes");
 
 // questions array
 const questions = [
     {
-      type: 'input',
-      message: 'What is the text you would like to be in your logo? (up to 3 chars)',
-      name: 'text',
+        type: 'input',
+        message: 'What is the text you would like to be in your logo? (up to 3 chars)',
+        name: 'text',
     },
     {
         type: 'input',
@@ -32,13 +32,13 @@ function generateSVG(shape, text, colorText, colorLogo) {
     let newShape;
 
     // assigns to shape class per user input
-    if(shape === 'square') {
+    if (shape === 'square') {
         newShape = new Square(colorLogo);
     }
-    if(shape === 'circle') {
+    if (shape === 'circle') {
         newShape = new Circle(colorLogo);
     }
-    if(shape === 'triangle') {
+    if (shape === 'triangle') {
         newShape = new Triangle(colorLogo);
     }
 
@@ -55,19 +55,19 @@ function generateSVG(shape, text, colorText, colorLogo) {
 // initializes the app
 function init() {
     inquirer.prompt(questions)
-    .then(data => {
-        console.log(data);
+        .then(data => {
+            console.log(data);
 
-        // Checks to see that the password is 3 chars or less
-        if(data.text.length > 3) {
-           return console.log('Error: Please enter 3 or less characters');
-        }
+            // Checks to see that the password is 3 chars or less
+            if (data.text.length > 3) {
+                return console.log('Error: Please enter 3 or less characters');
+            }
 
-        generateSVG(data.shape, data.text, data.colorText, data.colorLogo);
-        // writes svg file
+            generateSVG(data.shape, data.text, data.colorText, data.colorLogo);
+            // writes svg file
             fs.writeFile('./examples/logo.svg', generateSVG(data.shape, data.text, data.colorText, data.colorLogo), (err) => {
-                err ? console.log(err) : console.log('Success! Check out your new logo.svg.') 
-    });
+                err ? console.log(err) : console.log('Success! Check out your new logo.svg.')
+            });
         })
 }
 
